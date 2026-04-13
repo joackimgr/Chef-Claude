4d216a0190e4b7f6008a1f2d249cdde361b4a1a7
Joackim Pantelakis <joackim.gr@gmail.com>
2026-04-13 19:58:21 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import chef from "../assets/chef.png"
+
+export default function Header() {
+
+    return <header>
+            <img src={chef} alt="chef-icon" />
+            <h2>Chef Claude</h2>
+        </header>
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    
+    return (
+        <main>
+            <form>
+                <input type="text" placeholder="e.g oregano" aria-label="Add ingredient"/>
+                <input type="submit" value="+ Add ingredient"/>
+            </form>
+        </main>
+    )
+}
