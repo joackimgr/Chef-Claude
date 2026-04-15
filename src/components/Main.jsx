d9224d89b9f5c5fe506a70d4139a6b05ba5945da
Joackim Pantelakis <joackim.gr@gmail.com>
2026-04-15 20:28:56 +0300

    Second Commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,29 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const ingredientsListItems = ingredients.map(item => (<li key={item}>{item}</li>))
+    
+    function handleSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
     
     return (
         <main>
-            <form>
-                <input type="text" placeholder="e.g oregano" aria-label="Add ingredient"/>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="e.g oregano" 
+                    aria-label="Add ingredient"
+                    name = "ingredient"    
+                />
                 <input type="submit" value="+ Add ingredient"/>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
