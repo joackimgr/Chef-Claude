214643ba3637e78dd3f45702e4ef207174e6db0d
Joackim Pantelakis <joackim.gr@gmail.com>
2026-04-30 20:18:31 +0300

    Split every section to its own component

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,21 @@
+export default function IngredientsList(props) {
+
+    const ingredientsListItems = props.ingredientsArray.map(item => (<li key={item}>{item}</li>))
+
+    return (
+       <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className='ingredientsList' aria-live='polite'>
+                {ingredientsListItems}
+            </ul>
+                {props.ingredientsArray.length >= 4 && 
+                <div className="getRecipeContainer">
+                    <div className='generateRecipeText'>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.toggleRecipe}>Get a recipe.</button>
+                </div>}
+        </section> 
+    )
+}
